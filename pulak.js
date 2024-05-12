3a9c3d3d52f38d02fce1b634f4d22f2919d7c59f
Pulsyco <pulakdas32413@gmail.com>
2024-05-12 12:30:05 +0530

    test

---
+++ pulak.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const pulak = () => {
+  return (
+    <View>
+      <Text>pulak</Text>
+    </View>
+  )
+}
+
+export default pulak
